a484105bb01d677826ff786d0590fa3ff3b2a333
Hiruni28 <hiruniparindhya28@gmail.com>
2026-06-09 21:40:17 +0530

    Staff model

--- queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/model/Staff.java
+++ queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/model/Staff.java
@@ -1,37 +1,29 @@
 package com.hospital.queue_management_system.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-import lombok.*;
-
-@Entity
-
-@Table(
-        name="Staff"
-)
-
-@Getter
 @Setter
-
-@NoArgsConstructor
-
-@AllArgsConstructor
+@Getter
+@Entity
+@Table(name="staff")
 
 public class Staff {
 
     @Id
-
-    @GeneratedValue(
-            strategy=
-                    GenerationType.IDENTITY
-    )
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
 
     private Long staffId;
 
     private String fullName;
 
+    @Column(unique=true)
+
     private String email;
 
     private String password;
+
+    public Staff(){}
 
 }
