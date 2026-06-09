6515ffad2cc9c427428706f43583d43450b11ee1
Hiruni28 <hiruniparindhya28@gmail.com>
2026-06-09 17:40:18 +0530

    Doctor repository

--- queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/repository/DoctorRepository.java
+++ queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/repository/DoctorRepository.java
@@ -1,7 +1,6 @@
 package com.hospital.queue_management_system.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.*;
 import com.hospital.queue_management_system.model.Doctor;
 
 public interface DoctorRepository
