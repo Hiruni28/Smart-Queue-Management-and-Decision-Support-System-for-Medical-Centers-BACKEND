ac5ef476c0e5f3620622946cbc441491f807810a
Hiruni28 <hiruniparindhya28@gmail.com>
2026-06-13 06:09:37 +0530

    Appointment Service file

---
+++ queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/service/AppointmentService.java
@@ -0,0 +1,58 @@
+package com.hospital.queue_management_system.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.hospital.queue_management_system.model.Appointment;
+import com.hospital.queue_management_system.repository.AppointmentRepository;
+
+@Service
+
+public class AppointmentService {
+
+    private final AppointmentRepository repo;
+
+    public AppointmentService(
+            AppointmentRepository repo
+    ){
+
+        this.repo=repo;
+
+    }
+
+    public Appointment book(
+            Appointment appointment
+    ){
+
+        appointment.setStatus(
+                "Booked"
+        );
+
+        return repo.save(
+                appointment
+        );
+
+    }
+
+    public List<Appointment> patientAppointments(
+            Long patientId
+    ){
+
+        return repo.findByPatientId(
+                patientId
+        );
+
+    }
+
+    public void cancel(
+            Long id
+    ){
+
+        repo.deleteById(
+                id
+        );
+
+    }
+
+}
