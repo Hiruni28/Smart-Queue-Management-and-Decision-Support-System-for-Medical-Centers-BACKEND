338d701b7cab65599ae61b3b84caa99552c601a5
Hiruni28 <hiruniparindhya28@gmail.com>
2026-06-13 06:10:21 +0530

    Appointment Repository file

--- queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/repository/AppointmentRepository.java
+++ queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/repository/AppointmentRepository.java
@@ -1,10 +1,16 @@
 package com.hospital.queue_management_system.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hospital.queue_management_system.model.Appointment;
 
 public interface AppointmentRepository
         extends JpaRepository<Appointment,Long>{
+
+    List<Appointment> findByPatientId(
+            Long patientId
+    );
 
 }
