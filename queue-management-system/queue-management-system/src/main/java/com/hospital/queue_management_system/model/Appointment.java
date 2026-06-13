96324b70e7c330979b726fef3bbc177a592fcd4e
Hiruni28 <hiruniparindhya28@gmail.com>
2026-06-13 06:10:46 +0530

    Appointment model file

--- queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/model/Appointment.java
+++ queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/model/Appointment.java
@@ -1,41 +1,43 @@
 package com.hospital.queue_management_system.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.time.LocalDate;
-
-import lombok.*;
-
-@Entity
-
-@Table(
-        name="Appointments"
-)
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
 
 @Getter
 @Setter
+@Entity
 
-@NoArgsConstructor
-
-@AllArgsConstructor
+@Table(name="Appointments")
 
 public class Appointment {
 
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
 
-    @GeneratedValue(
-            strategy=
-                    GenerationType.IDENTITY
-    )
-
+    @Column(name="appointment_id")
     private Long appointmentId;
 
+    @Column(name="patient_id")
     private Long patientId;
 
+    @Column(name="doctor_id")
     private Long doctorId;
 
-    private LocalDate appointmentDate;
+    @Column(name="appointment_date")
+    private Date appointmentDate;
 
+    @Column(name="appointment_time")
+    private Time appointmentTime;
+
+    @Column(name="status")
     private String status;
+
+    @Column(name="created_at")
+    private Timestamp createdAt;
 
 }
