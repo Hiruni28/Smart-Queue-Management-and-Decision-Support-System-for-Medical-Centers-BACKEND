e18c67674fab2fde232f83470d237aba59a19711
Hiruni28 <hiruniparindhya28@gmail.com>
2026-06-16 21:30:41 +0530

    Staff Service file

--- queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/service/StaffService.java
+++ queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/service/StaffService.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import org.springframework.stereotype.Service;
 
+import com.hospital.queue_management_system.dto.LoginRequest;
 import com.hospital.queue_management_system.model.Staff;
 import com.hospital.queue_management_system.repository.StaffRepository;
 
@@ -16,34 +17,47 @@ public class StaffService {
     public StaffService(
             StaffRepository repo
     ){
-
         this.repo=repo;
-
     }
 
     public List<Staff> getAll(){
-
         return repo.findAll();
-
     }
 
     public Staff save(
             Staff staff
     ){
-
-        return repo.save(
-                staff
-        );
-
+        return repo.save(staff);
     }
 
     public void delete(
             Long id
     ){
+        repo.deleteById(id);
+    }
 
-        repo.deleteById(
-                id
-        );
+    public boolean login(
+            LoginRequest request
+    ){
+
+        return repo
+                .findByEmail(
+                        request.getEmail()
+                )
+
+                .map(
+
+                        staff->
+
+                                staff
+                                        .getPassword()
+                                        .equals(
+                                                request.getPassword()
+                                        )
+
+                )
+
+                .orElse(false);
 
     }
 
