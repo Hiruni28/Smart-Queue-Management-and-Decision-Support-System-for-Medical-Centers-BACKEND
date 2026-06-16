7e011273ba48369f1762c904b1f3a9988b9e56f4
Hiruni28 <hiruniparindhya28@gmail.com>
2026-06-16 21:31:01 +0530

    Staff Repository file

--- queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/repository/StaffRepository.java
+++ queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/repository/StaffRepository.java
@@ -1,10 +1,16 @@
 package com.hospital.queue_management_system.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hospital.queue_management_system.model.Staff;
 
 public interface StaffRepository
-        extends JpaRepository<Staff,Long>{
+        extends JpaRepository<Staff, Long>{
+
+    Optional<Staff> findByEmail(
+            String email
+    );
 
 }
