9835875d05e030c313e76c7677eb0024accf209e
Hiruni28 <hiruniparindhya28@gmail.com>
2026-06-17 16:35:24 +0530

    Updated Doctor Repository file

--- queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/repository/DoctorRepository.java
+++ queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/repository/DoctorRepository.java
@@ -1,9 +1,16 @@
 package com.hospital.queue_management_system.repository;
 
-import org.springframework.data.jpa.repository.*;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.hospital.queue_management_system.model.Doctor;
 
 public interface DoctorRepository
         extends JpaRepository<Doctor,Long>{
+
+    Optional<Doctor> findById(
+            Long id
+    );
 
 }
