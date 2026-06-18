a0b12de12155e1f0c5271e8611b876cabb5af989
Hiruni28 <hiruniparindhya28@gmail.com>
2026-06-19 01:30:19 +0530

    Updated Doctor Schedule model file

--- queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/model/DoctorSchedule.java
+++ queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/model/DoctorSchedule.java
@@ -1,44 +1,56 @@
 package com.hospital.queue_management_system.model;
 
 import jakarta.persistence.*;
+
 import lombok.Getter;
 import lombok.Setter;
 
-import java.sql.Date;
-import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Setter
 @Getter
-@Entity
 
+@Entity
 @Table(name="doctor_schedules")
 
 public class DoctorSchedule {
 
     @Id
-
     @GeneratedValue(
-            strategy=GenerationType.IDENTITY
+            strategy = GenerationType.IDENTITY
     )
 
     @Column(name="schedule_id")
-
     private Long scheduleId;
 
     @Column(name="doctor_id")
-
     private Long doctorId;
+
+    @JsonFormat(
+            pattern="yyyy-MM-dd"
+    )
 
     @Column(name="available_date")
 
-    private Date availableDate;
+    private LocalDate availableDate;
+
+    @JsonFormat(
+            pattern="HH:mm:ss"
+    )
 
     @Column(name="start_time")
 
-    private Time startTime;
+    private LocalTime startTime;
+
+    @JsonFormat(
+            pattern="HH:mm:ss"
+    )
 
     @Column(name="end_time")
 
-    private Time endTime;
+    private LocalTime endTime;
 
 }
