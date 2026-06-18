b0fc8f04911e29c8eb717ae7cfd8160777fdf749
Hiruni28 <hiruniparindhya28@gmail.com>
2026-06-19 01:30:34 +0530

    Updated Doctor Schedule controller file

--- queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/controller/DoctorScheduleController.java
+++ queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/controller/DoctorScheduleController.java
@@ -8,11 +8,10 @@ import com.hospital.queue_management_system.model.DoctorSchedule;
 import com.hospital.queue_management_system.service.DoctorScheduleService;
 
 @RestController
-
 @RequestMapping("/schedule")
 
 @CrossOrigin(
-        origins="http://localhost:5173"
+        origins = "http://localhost:5173"
 )
 
 public class DoctorScheduleController {
@@ -21,23 +20,67 @@ public class DoctorScheduleController {
 
     public DoctorScheduleController(
             DoctorScheduleService service
-    ){
+    ) {
+        this.service = service;
+    }
 
-        this.service=service;
+    @GetMapping
+    public List<DoctorSchedule> getAll() {
+
+        return service.getAll();
 
     }
 
     @GetMapping("/{doctorId}")
-
     public List<DoctorSchedule> get(
-
             @PathVariable
             Long doctorId
-
-    ){
+    ) {
 
         return service.getByDoctor(
                 doctorId
+        );
+
+    }
+
+    @PostMapping
+    public DoctorSchedule add(
+
+            @RequestBody
+            DoctorSchedule schedule
+
+    ) {
+
+        return service.add(
+                schedule
+        );
+
+    }
+
+    @PutMapping
+    public DoctorSchedule update(
+
+            @RequestBody
+            DoctorSchedule schedule
+
+    ) {
+
+        return service.update(
+                schedule
+        );
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(
+
+            @PathVariable
+            Long id
+
+    ) {
+
+        service.delete(
+                id
         );
 
     }
