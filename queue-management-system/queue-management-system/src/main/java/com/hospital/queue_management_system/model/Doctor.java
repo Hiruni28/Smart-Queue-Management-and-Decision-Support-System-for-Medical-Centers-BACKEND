9a4e115ddb15d1420b9c1e133707eb9bfe34fb4a
Hiruni28 <hiruniparindhya28@gmail.com>
2026-07-05 06:51:19 +0530

    Updated Doctor model file

--- queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/model/Doctor.java
+++ queue-management-system/queue-management-system/src/main/java/com/hospital/queue_management_system/model/Doctor.java
@@ -1,51 +1,36 @@
 package com.hospital.queue_management_system.model;
 
 import jakarta.persistence.*;
-
 import lombok.*;
 
 @Entity
-
-@Table(
-        name="Doctors"
-)
+@Table(name = "Doctors")
 
 @Getter
 @Setter
-
 @NoArgsConstructor
-
 @AllArgsConstructor
 
 public class Doctor {
 
     @Id
-
     @GeneratedValue(
-            strategy=
-                    GenerationType.IDENTITY
+            strategy = GenerationType.IDENTITY
     )
-
-    @Column(
-            name="doctor_id"
-    )
-
+    @Column(name = "doctor_id")
     private Long doctorId;
 
-    @Column(
-            name="doctor_name"
-    )
-
+    @Column(name = "doctor_name")
     private String doctorName;
 
     private String specialization;
 
     private String availability;
 
-    @Column(
-            name="room_number"
-    )
-
+    @Column(name = "room_number")
     private String roomNumber;
+
+    @Column(name = "max_patients_per_day")
+    private Integer maxPatientsPerDay;
 
 }
